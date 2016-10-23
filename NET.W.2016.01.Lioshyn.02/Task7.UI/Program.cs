fab50c8a75ff571f47418ba63ae8f1f34e21d629
Maksim Lioshyn <leshinmaxim@gmail.com>
2016-10-23 20:28:10 +0300

    Day 2. Task 5, 6 Complete, Task 7 50% complele

---
+++ NET.W.2016.01.Lioshyn.02/Task7.UI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task7;
+
+namespace Task7.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertBinary insertBinary = new InsertBinary(8, 15, 0, 0);
+            insertBinary.PrintNumber();
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ NET.W.2016.01.Lioshyn.02/Task7/InsertBinary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    public class InsertBinary
+    {
+        private int _firstNumber;
+        private int _secondNumber;
+        private int _i;
+        private int _j;
+        private string _str;
+
+        public InsertBinary(int first, int second, int i, int j)
+        {
+            _firstNumber = first;
+            _secondNumber = second;
+            _i = i;
+            _j = j;
+        }
+
+        private int BinaryToInt(string str)
+        {
+            return Convert.ToInt32(str, 10);
+        }
+
+        private string IntToBinary(int number)
+        {
+            return Convert.ToString(number, 2);
+        }
+
+        public void Insertion()
+        {
+
+        }
+
+        public void PrintNumber()
+        {
+            string str = IntToBinary(_firstNumber);
+            int i = BinaryToInt(str) + 1;
+            Console.Write(i.ToString());
+        }
+    }
+}
