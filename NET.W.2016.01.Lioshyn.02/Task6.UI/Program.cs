0497c6a958a53d3be2383f29a54f3a2e7a3ec1af
Maksim Lioshyn <leshinmaxim@gmail.com>
2016-10-23 18:22:16 +0300

    Task 5,6 complete

---
+++ NET.W.2016.01.Lioshyn.02/Task6.UI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task6;
+
+namespace Task6.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string str1 = "", str2 = "";
+
+            str1 = Console.ReadLine();
+            str2 = Console.ReadLine();
+
+            //str1 = "xyaabbbccccdefww";
+            //str2 = "xxxxyyyyabklmopq";
+
+            //str1 = "abcdefghijklmnopqrstuvwxyz";
+            //str2 = str1;
+
+            ConCatSortClass conCatSortClass = new ConCatSortClass(str1, str2);
+            conCatSortClass.GeneralFunction();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NET.W.2016.01.Lioshyn.02/Task6/ConCatSortClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    public class ConCatSortClass
+    {
+        private readonly string _startStr;
+        private string _finishStr;
+
+        /// <summary>
+        /// The class constructor.
+        /// </summary>
+        /// <param name="str1">First string.</param>
+        /// <param name="str2">Second string.</param>
+
+        public ConCatSortClass(string str1, string str2)
+        {
+            _startStr = str1 + str2;
+            _finishStr = "";
+        }
+
+        /// <summary>
+        /// String concatenation and Delete duplicate chars
+        /// </summary>
+        /// <returns>line string </returns>
+        private string ConCatDeleteDuplicate()
+        {
+            string str = "";
+
+            for (int i = 97; i <= 122; i++)
+            {
+                if (_startStr.IndexOf((char)i) > -1)
+                {
+                    str += (char) i;
+                }
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// Sort line small laters order by ABC and show it.
+        /// </summary>
+
+        public void GeneralFunction()
+        {
+            _finishStr = ConCatDeleteDuplicate();
+            _finishStr = string.Concat(_finishStr.OrderBy(x => x).ToArray());
+            Console.WriteLine(_finishStr);
+        }
+    }
+}
