30f600fcd4935d81bac3aacf65eb45e03ae0061d
Maksim Lioshyn <leshinmaxim@gmail.com>
2016-10-25 00:05:44 +0300

    Day2. Task 5-7 Complete

--- NET.W.2016.01.Lioshyn.02/Task7.UI/Program.cs
+++ NET.W.2016.01.Lioshyn.02/Task7.UI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,16 @@ namespace Task7.UI
     {
         static void Main(string[] args)
         {
-            InsertBinary insertBinary = new InsertBinary(8, 15, 0, 0);
-            insertBinary.PrintNumber();
+            InsertBinary insertBinary0 = new InsertBinary(8, 15, 0, 0);
+            InsertBinary insertBinary1 = new InsertBinary(0, 15, 30, 30);
+            InsertBinary insertBinary2 = new InsertBinary(0, 15, 0, 30);
+            InsertBinary insertBinary3 = new InsertBinary(15, -15, 4, 4);
+            InsertBinary insertBinary4 = new InsertBinary(15, int.MaxValue, 3, 5);
+            insertBinary0.Insertion();
+            insertBinary1.Insertion();
+            insertBinary2.Insertion();
+            insertBinary3.Insertion();
+            insertBinary4.Insertion();
 
             Console.ReadKey();
         }
--- NET.W.2016.01.Lioshyn.02/Task7/InsertBinary.cs
+++ NET.W.2016.01.Lioshyn.02/Task7/InsertBinary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,40 +9,67 @@ namespace Task7
 {
     public class InsertBinary
     {
-        private int _firstNumber;
-        private int _secondNumber;
-        private int _i;
-        private int _j;
-        private string _str;
+        private BitArray firstNumberBitArray;
+        private BitArray secondNumberBitArray;
+        private int i;
+        private int j;
 
         public InsertBinary(int first, int second, int i, int j)
         {
-            _firstNumber = first;
-            _secondNumber = second;
-            _i = i;
-            _j = j;
-        }
-
-        private int BinaryToInt(string str)
-        {
-            return Convert.ToInt32(str, 10);
-        }
-
-        private string IntToBinary(int number)
-        {
-            return Convert.ToString(number, 2);
+            firstNumberBitArray = ToBinary(first);
+            secondNumberBitArray = ToBinary(second);
+            this.i = i;
+            this.j = j;
         }
 
         public void Insertion()
         {
+            PrintBitArray(firstNumberBitArray);
+            Console.WriteLine();
+            PrintBitArray(secondNumberBitArray);
+            Console.WriteLine();
+
+            for (int index1 = this.i, index2 = 0; index1 <= this.j; index1++, index2++)
+            {
+                firstNumberBitArray[index1] = secondNumberBitArray[index2];
+            }
+
+            
+            PrintBitArray(firstNumberBitArray);
+            Console.WriteLine();
+            PrintBitArray(secondNumberBitArray);
+            Console.WriteLine();
+
+            Console.WriteLine(ToNumeral(firstNumberBitArray));
+
 
         }
 
-        public void PrintNumber()
+        public void PrintBitArray(BitArray bitArray)
         {
-            string str = IntToBinary(_firstNumber);
-            int i = BinaryToInt(str) + 1;
-            Console.Write(i.ToString());
+            StringBuilder sb = new StringBuilder();
+            foreach (var b in bitArray)
+            {
+                sb.Append((bool)b ? "1" : "0");
+            }
+            Console.Write(sb.ToString());
+        }
+
+        private BitArray ToBinary(int numeral)
+        {
+            return new BitArray(new[] { numeral });
+        }
+
+        private long ToNumeral(BitArray binary)
+        {
+            if (binary == null)
+                throw new ArgumentNullException("binary");
+            if (binary.Length > 32)
+                throw new ArgumentException("must be at most 32 bits long");
+
+            var result = new int[1];
+            binary.CopyTo(result, 0);
+            return result[0];
         }
     }
 }
